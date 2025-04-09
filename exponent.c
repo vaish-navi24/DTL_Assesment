4e1cb73bb9c14c32b8058d04cfc13a91e332e6ff
vaish-navi24 <shambharkarv84@gmail.com>
2025-04-09 10:53:41 +0530

    Hexadecimal code

--- exponent.c
+++ exponent.c
@@ -3,6 +3,7 @@
 int main() {
     int a, b;
     char op;
+    double result;
 
     printf("Enter an expression (e.g. 10 + 5): ");
     scanf("%d %c %d", &a, &op, &b);
@@ -19,6 +20,28 @@ int main() {
             break;
         default: printf("Invalid operator\n");
     }
+    
+    printf("Enter hex expression (e.g. A + F): ");
+    scanf("%x %c %x", &a, &op, &b);
+
+    switch (op) {
+        case '+': result = a + b; break;
+        case '-': result = a - b; break;
+        case '*': result = a * b; break;
+        case '/':
+            if (b != 0)
+                result = a / b;
+            else {
+                printf("Error: Division by zero\n");
+                return 1;
+            }
+            break;
+        default:
+            printf("Invalid operator\n");
+            return 1;
+    }
+
+    printf("Result in hex: %X\n", result);
 
     return 0;
 }
